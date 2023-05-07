c6ee1772af2b8b53aa2402a9c128cdd9b04865f7
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-05-06 23:13:57 -0400

    Model changes

--- models/index.js
+++ models/index.js
@@ -4,9 +4,15 @@ const Category = require("./Category");
 const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
-// Products belongsTo Category
-
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
@@ -14,7 +20,7 @@ Product.belongsToMany(Tag, {
     model: ProductTag,
     unique: false,
   },
-  // as:
+  as: "product_tags",
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
@@ -22,7 +28,7 @@ Tag.belongsToMany(Product, {
     model: ProductTag,
     unique: false,
   },
-  // as:
+  as: "tag_product",
 });
 
 module.exports = {
